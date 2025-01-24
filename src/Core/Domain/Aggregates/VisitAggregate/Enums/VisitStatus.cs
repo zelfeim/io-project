61330f00b8eb38cc1506f30870815d473956a3e1
zelfeim <zelfeim@mailbox.org>
2025-01-24 23:36:04 +0100

    add controllers for visit aggregate

--- src/Core/Domain/Aggregates/VisitAggregate/Enums/VisitStatus.cs
+++ src/Core/Domain/Aggregates/VisitAggregate/Enums/VisitStatus.cs
@@ -4,5 +4,5 @@ public enum VisitStatus
 {
     Planned,
     Completed,
-    Canceled
+    Cancelled
 }
---
+++ src/Core/Features/Visit/RescheduleVisit/RescheduleVisitRequest.cs
@@ -0,0 +1,7 @@
+namespace Application.Features.Visit.RescheduleVisit;
+
+public class RescheduleVisitRequest
+{
+    public DateTime Date;
+    public int VisitLength;
+}
