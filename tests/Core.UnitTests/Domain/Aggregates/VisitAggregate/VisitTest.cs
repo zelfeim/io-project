70553f586d620ca13b7aa3ade3661805823175b7
zelfeim <zelfeim@mailbox.org>
2025-01-25 00:40:09 +0100

    add tests for RescheduleVisit

--- tests/Core.UnitTests/Domain/Aggregates/VisitAggregate/VisitTest.cs
+++ tests/Core.UnitTests/Domain/Aggregates/VisitAggregate/VisitTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Application.Domain.Aggregates.VisitAggregate;
 using Application.Domain.Aggregates.VisitAggregate.Enums;
 using FluentAssertions;
@@ -36,5 +37,40 @@ public class VisitTest
         // Assert
         act.Should().Throw<Exception>()
             .WithMessage("It's not possible to change visit status from Completed to Cancelled.");
+    }
+
+    [Fact]
+    public void RescheduleVisitShouldRescheduleVisit()
+    {
+        // Arrange
+        var visit = new Visit(0, 0, DateTime.Now.AddHours(1), VisitType.Examination, 30);
+        
+        // Act
+        visit.RescheduleVisit(DateTime.Parse("2035-01-01"), 60);
+
+        // Assert
+        visit.VisitStatus.Should().Be(VisitStatus.Planned);
+        visit.Date.Should().Be(DateTime.Parse("2035-01-01"));
+        visit.VisitLength.Should().Be(60);
+    }
+
+    public static TheoryData<DateTime, int, string> RescheduleVisitTestData = new()
+    {
+        { DateTime.Now, 30, "Visit date must be in the future (Parameter 'date')" },
+        { DateTime.Now.AddHours(1), 0, "Visit length must be greater than zero (Parameter 'visitLength')" },
+    };
+    
+    [Theory]
+    [MemberData(nameof(RescheduleVisitTestData))]
+    public void RescheduleVisitShouldThrowExceptionWhenValidationFails(DateTime date, int length, string expectedMessage)
+    {
+        // Arrange
+        var visit = new Visit(0, 0, DateTime.Now.AddHours(1), VisitType.Examination, 30);
+        
+        // Act
+        var act = () => visit.RescheduleVisit(date, length);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage(expectedMessage);
     }
 }
