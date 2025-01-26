13f8dbcfc3e1fd4a9792854a2d5f0fd34699f5fb
zelfeim <zelfeim@mailbox.org>
2025-01-27 00:40:11 +0100

    add get animal owner controller

---
+++ src/Core/Features/AnimalOwner/GetAnimalOwner/GetAnimalOwnerController.cs
@@ -0,0 +1,27 @@
+using Application.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.AnimalOwner.GetAnimalOwner;
+
+[ApiController]
+[Route("api/animal-owner")]
+public class GetAnimalOwnerController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GetAnimalOwnerController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<GetAnimalOwnerResponse>> Handle(int id)
+    {
+        var animalOwner = await _dbContext.AnimalOwners.SingleOrDefaultAsync(o => o.Id == id);
+        
+        if(animalOwner == null) return NotFound();
+        
+        return Ok(GetAnimalOwnerMapper.MapAnimalOwnerToGetAnimalOwnerResponse(animalOwner));
+    } 
+}
---
+++ src/Core/Features/AnimalOwner/GetAnimalOwner/GetAnimalOwnerMapper.cs
@@ -0,0 +1,18 @@
+namespace Application.Features.AnimalOwner.GetAnimalOwner;
+
+public static class GetAnimalOwnerMapper
+{
+    public static GetAnimalOwnerResponse MapAnimalOwnerToGetAnimalOwnerResponse(
+        Domain.Aggregates.AnimalOwnerAggregate.AnimalOwner animalOwner)
+    {
+        return new GetAnimalOwnerResponse
+        {
+            Id = animalOwner.Id,
+            Name = animalOwner.Name,
+            Surname = animalOwner.Surname,
+            Email = animalOwner.Email,
+            Address = animalOwner.Address,
+            Telephone = animalOwner.Telephone,
+        };
+    }
+}
