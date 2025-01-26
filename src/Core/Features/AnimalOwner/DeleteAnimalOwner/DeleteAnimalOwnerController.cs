fe28995b9951a2f0c36090eff818159e94a9b886
zelfeim <zelfeim@mailbox.org>
2025-01-27 00:58:09 +0100

    add DeleteAnimalOwnerController.cs

---
+++ src/Core/Features/AnimalOwner/DeleteAnimalOwner/DeleteAnimalOwnerController.cs
@@ -0,0 +1,29 @@
+using Application.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Features.AnimalOwner.DeleteAnimalOwner;
+
+[ApiController]
+[Route("api/animal-owner")]
+public class DeleteAnimalOwnerController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public DeleteAnimalOwnerController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Handle(int id)
+    {
+        var animalOwner = await _dbContext.AnimalOwners.FindAsync(id);
+
+        if (animalOwner == null) return NotFound();
+
+        _dbContext.AnimalOwners.Remove(animalOwner);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
