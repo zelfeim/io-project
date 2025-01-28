084d2b1a686faec6f8083df36181a298cd81b31d
zelfeim <zelfeim@mailbox.org>
2025-01-28 20:21:45 +0100

    add missing migrationss

---
+++ src/Core/Infrastructure/Migrations/20250127223735_ChangesToEmployeeAggregate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesToEmployeeAggregate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Employees",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress_Email",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Employees",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress_Email",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
