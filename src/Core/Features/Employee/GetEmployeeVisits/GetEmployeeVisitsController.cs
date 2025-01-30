5a07fc42c6b3428b9ecdef098bef9b778601c05d
zelfeim <69108769+zelfeim@users.noreply.github.com>
2025-01-30 19:44:36 +0000

    Get current employee visits (#24)

--- src/Core/Features/Employee/GetEmployeeVisits/GetEmployeeVisitsController.cs
+++ src/Core/Features/Employee/GetEmployeeVisits/GetEmployeeVisitsController.cs
@@ -1,8 +1,10 @@
+using System.Security.Claims;
 using Application.Features.Visit.GetVisit;
 using Application.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Application.Features.Employee.GetEmployeeVisits;
 
@@ -20,12 +22,24 @@ public class GetEmployeeVisitsController : ControllerBase
     }
 
     [HttpGet("{id:int}/visits")]
-    [Authorize(Roles = "Admin,Vet,Receptionist")]
+    [Authorize(Roles = "Admin,Receptionist")]
+    [SwaggerOperation(Tags = ["Employee"])]
     public ActionResult<IEnumerable<GetVisitResponse>> GetEmployeeVisits([FromRoute] int id)
     {
         _logger.LogInformation("{Controller} called", nameof(GetEmployeeVisits));
 
-        var visits = _dbContext.Visits.Where(v => v.Id == id).ToList();
+        var visits = _dbContext.Visits.Where(v => v.EmployeeId == id).ToList();
+
+        return Ok(visits.Select(GetVisitMapper.MapVisitToGetVisitResponse));
+    }
+
+    [HttpGet("visits")]
+    [SwaggerOperation(Tags = ["Employee"])]
+    public ActionResult<IEnumerable<GetVisitResponse>> GetCurrentEmployeeVisits()
+    {
+        var id = HttpContext.User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier);
+
+        var visits = _dbContext.Visits.Where(v => v.EmployeeId == Convert.ToInt32(id)).ToList();
 
         return Ok(visits.Select(GetVisitMapper.MapVisitToGetVisitResponse));
     }
