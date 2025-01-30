ff9f71d7a988880e4fd671c2a3f236f2dfed33ce
zelfeim <zelfeim@mailbox.org>
2025-01-30 19:46:58 +0100

    Revert "Merge pull request #20"
    This reverts commit 7aa89c9a7f281da8f41e739b1b5f19992223ae59.

--- src/Core/Features/Resources/GetResource/GetResourceResponse.cs
+++ src/Core/Features/Resources/GetResource/GetResourceResponse.cs
@@ -1,11 +1,9 @@
-using Application.Domain.Aggregates.ResourcesAggregate;
-
 namespace Application.Features.Resources.GetResource;
 
 public record GetResourceResponse
 {
     public required string Name { get; init; }
-    public ResourceType Type { get; init; }
+    public required string Type { get; init; }
     public int Amount { get; init; }
     public DateTime ShelfLive { get; init; }
     public int Id { get; init; }
